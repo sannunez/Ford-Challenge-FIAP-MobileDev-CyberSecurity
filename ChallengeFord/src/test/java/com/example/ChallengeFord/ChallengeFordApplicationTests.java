ee8da7831d960f6cc23161756e162d997b262c0c
Z4ffarani <zaffarani.kaique@gmail.com>
2026-05-21 14:17:49 -0300

    Subindo Back-End

---
+++ ChallengeFord/src/test/java/com/example/ChallengeFord/ChallengeFordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChallengeFord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeFordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
